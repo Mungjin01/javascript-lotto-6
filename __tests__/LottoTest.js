669a85bab9251f31193b4303e37c3425d0e2712c
Mungjin01 <beckaljm@naver.com>
2023-11-06 14:20:14 +0900

    test: 로또 번호 예외 처리 테스트 코드

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,34 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  describe('Lotto 클래스', () => {
+    test('올바른 로또 번호로 인스턴스 생성', () => {
+      const validLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      expect(validLotto).toBeInstanceOf(Lotto);
+    });
+  
+    test('6개의 숫자가 아닌 경우 예외 발생', () => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5]);
+      }).toThrow('[ERROR] 로또 번호는 6개여야 합니다.');
+    });
+  
+    test('1부터 45 범위의 숫자가 아닌 경우 예외 발생', () => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, 70]);
+      }).toThrow('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다. (잘못된 번호: 70)');
+    });
+  
+    test('소수가 포함된 경우 예외 발생', () => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, 1.7]);
+      }).toThrow('[ERROR] 로또 번호는 정수여야 합니다. (잘못된 번호: 1.7)');
+    });
+  
+    test('중복된 숫자가 포함된 경우 예외 발생', () => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, 5]);
+      }).toThrow('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    });
+  });
 });
