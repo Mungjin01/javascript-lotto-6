820e9ae6ce19b555ab68bcdabaa644dd2189957c
Mungjin01 <beckaljm@naver.com>
2023-11-06 13:43:37 +0900

    feat: 로또 번호 예외 처리 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,56 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const uniqueNumbers = new Set(numbers);
+
+    if (uniqueNumbers.size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+
+    for (const number of numbers) {
+      if (typeof number !== 'number' || number < 1 || number > 45) {
+        throw new Error(`[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다. (잘못된 번호: ${number})`);
+      }
+
+      if (number % 1 !== 0) {
+        throw new Error(`[ERROR] 로또 번호는 정수여야 합니다. (잘못된 번호: ${number})`);
+      }
+    }
   }
 
   // TODO: 추가 기능 구현
+}
+
+try {
+  const validLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+  console.log(validLotto); 
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  const invalidLotto1 = new Lotto([1, 2, 3, 4, 5]); 
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  const invalidLotto2 = new Lotto([1, 2, 3, 4, 5, 60]); 
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  const invalidLotto3 = new Lotto([1, 2, 3, 4, 5, 1.5]); 
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  const invalidLotto4 = new Lotto([1, 2, 3, 4, 5, 5]); 
+} catch (error) {
+  console.error(error.message);
 }
 
 export default Lotto;
