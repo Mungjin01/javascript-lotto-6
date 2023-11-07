59d8a752c457c8caa5c336b2a71f854cd93945df
Mungjin01 <beckaljm@naver.com>
2023-11-08 00:29:08 +0900

    feat: 로또 구입 금액 입력 예외 처리

--- src/input.js
+++ src/input.js
@@ -1,9 +1,29 @@
 import { Console } from '@woowacourse/mission-utils';
 
 async function getPurchaseAmount() {
-  const purchaseAmount = parseInt(await Console.readLineAsync('구입 금액을 입력해 주세요: '));
-  return purchaseAmount;
-}
+    let purchaseAmount;
+  
+    while (true) {
+      const input = await Console.readLineAsync('구입 금액을 입력해 주세요: ');
+  
+      if (!input.trim()) {
+        console.log("[ERROR] 입력값이 공백입니다. 다시 입력해 주세요.");
+        continue;
+      }
+  
+      purchaseAmount = parseInt(input);
+  
+      if (isNaN(purchaseAmount) || purchaseAmount % 1000 !== 0) {
+        console.log("[ERROR] 1000원 단위로 입력해 주세요. 다시 입력해 주세요.");
+        continue;
+      }
+  
+      break;
+    }
+  
+    return purchaseAmount;
+  }
+  
 
 async function getWinningNumbers() {
   const winningNumbers = (await Console.readLineAsync('당첨 번호를 입력해 주세요 (쉼표로 구분된 6개의 숫자): ')).split(',').map(Number);
