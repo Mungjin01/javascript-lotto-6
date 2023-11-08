310bb8a20ed6162bffe068715940550bdf62c957
Mungjin01 <beckaljm@naver.com>
2023-11-08 17:39:03 +0900

    test: 로또 당첨 번호 예외 처리

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,61 +1,67 @@
-import Lotto from "../src/Lotto.js";
+import getWinningNumbers from './getWinningNumbers.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+describe('getWinningNumbers 테스트', () => {
+  test('오류 없는 입력', async () => {
+    const originalReadLineAsync = global.Console.readLineAsync;
+    global.Console.readLineAsync = jest.fn().mockResolvedValue('1,2,3,4,5,6');
+
+    const winningNumbers = await getWinningNumbers();
+
+    expect(winningNumbers).toEqual([1, 2, 3, 4, 5, 6]);
+
+    global.Console.readLineAsync = originalReadLineAsync;
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+  test('숫자가 6개가 아닐 경우 예외를 던져야 함', async () => {
+    const originalReadLineAsync = global.Console.readLineAsync;
+    global.Console.readLineAsync = jest.fn().mockResolvedValue('1,2,3,4,5');
+
+    try {
+      await getWinningNumbers();
+    } catch (error) {
+      expect(error.message).toBe('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    global.Console.readLineAsync = originalReadLineAsync;
   });
 
-  // 아래에 추가 테스트 작성 가능
-  describe('로또 클래스 예외 처리 테스트', () => {
-    test('올바른 로또 번호로 인스턴스 생성', () => {
-      const validLotto = new Lotto([1, 2, 3, 4, 5, 6]);
-      expect(validLotto).toBeInstanceOf(Lotto);
-    });
-  
-    test('6개의 숫자가 아닌 경우 예외 발생', () => {
-      expect(() => {
-        new Lotto([1, 2, 3, 4, 5]);
-      }).toThrow('[ERROR] 로또 번호는 6개여야 합니다.');
-    });
-  
-    test('1부터 45 범위의 숫자가 아닌 경우 예외 발생', () => {
-      expect(() => {
-        new Lotto([1, 2, 3, 4, 5, 70]);
-      }).toThrow('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다. (잘못된 번호: 70)');
-    });
-  
-    test('소수가 포함된 경우 예외 발생', () => {
-      expect(() => {
-        new Lotto([1, 2, 3, 4, 5, 1.7]);
-      }).toThrow('[ERROR] 로또 번호는 정수여야 합니다. (잘못된 번호: 1.7)');
-    });
-  
-    test('중복된 숫자가 포함된 경우 예외 발생', () => {
-      expect(() => {
-        new Lotto([1, 2, 3, 4, 5, 5]);
-      }).toThrow('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
-    });
+  test('중복된 숫자가 있을 경우 예외를 던져야 한다', async () => {
+    const originalReadLineAsync = global.Console.readLineAsync;
+    global.Console.readLineAsync = jest.fn().mockResolvedValue('1,2,3,4,5,5');
+
+    try {
+      await getWinningNumbers();
+    } catch (error) {
+      expect(error.message).toBe('[ERROR] 로또 번호는 중복되지 않아야 한다');
+    }
+
+    global.Console.readLineAsync = originalReadLineAsync;
   });
 
-  describe('로또 클래스 오름차순 정렬 테스트', () => {
-    test('로또 번호가 오름차순으로 정렬되어야 한다', () => {
-      const lotto = new Lotto([5, 3, 6, 4, 1, 2]);
-      expect(lotto.getNumbers()).toStrictEqual([1, 2, 3, 4, 5, 6]);
-    });
+  test('숫자가 1부터 45 범위를 벗어날 경우 예외를 던져야 한다', async () => {
+    const originalReadLineAsync = global.Console.readLineAsync;
+    global.Console.readLineAsync = jest.fn().mockResolvedValue('1,2,3,4,5,60'); // 60은 1부터 45 범위를 벗어남
+
+    try {
+      await getWinningNumbers();
+    } catch (error) {
+      expect(error.message).toBe('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 한다 (잘못된 번호: 60)');
+    }
+
+    global.Console.readLineAsync = originalReadLineAsync;
   });
 
-  describe('로또 금액 입력 예외 처리 테스트', () =>{
-    test('입력값이 공백일 경우 예외 발생', () => {
-      
-    })
-  })
+  test('숫자가 정수가 아닐 경우 예외를 던져야 한다', async () => {
+    const originalReadLineAsync = global.Console.readLineAsync;
+    global.Console.readLineAsync = jest.fn().mockResolvedValue('1,2,3,4.5,5,6'); // 4.5는 정수가 아님
+
+    try {
+      await getWinningNumbers();
+    } catch (error) {
+      expect(error.message).toBe('[ERROR] 로또 번호는 정수여야 한다 (잘못된 번호: 4.5)');
+    }
+
+    global.Console.readLineAsync = originalReadLineAsync;
+  });
 });
+
